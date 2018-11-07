c6f27b225af76d71090a7dc2d99394a1326d9b0b
João Rodrigues <j.p.g.l.m.rodrigues@gmail.com>
2018-11-07 17:01:56 -0600

    Standardized pdb_merge.py to version2

---
+++ bin/pdb_merge.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python
+#
+# Copyright 2018 João M. C. Teixeira
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+Merges several PDB files into one. The contents are not sorted and no lines are
+deleted (e.g. END, TER statements) so we recommend piping the results through
+`pdb_tidy.py`.
+
+Usage:
+    python pdb_merge.py <pdb file> <pdb file>
+
+Example:
+    python pdb_merge.py 1ABC.pdb 1XYZ.pdb
+
+This program is part of the `pdb-tools` suite of utilities and should not be
+distributed isolatedly. The `pdb-tools` were created to quickly manipulate PDB
+files using the terminal, and can be used sequentially, with one tool streaming
+data to another. They are based on old FORTRAN77 code that was taking too much
+effort to maintain and compile. RIP.
+"""
+
+import os
+import sys
+
+__author__ = "Joao M.C. Teixeira"
+__email__ = "joaomcteixeira@gmail.com"
+
+
+def check_input(args):
+    """Checks whether to read from stdin/file and validates user input/options.
+    """
+
+    # Defaults
+    fl = []  # file list
+
+    if len(args) >= 1:
+        for fn in args:
+            if not os.path.isfile(fn):
+                emsg = 'ERROR!! File not found or not readable: \'{}\'\n'
+                sys.stderr.write(emsg.format(fn))
+                sys.stderr.write(__doc__)
+                sys.exit(1)
+
+            fh = open(fn, 'r')
+            fl.append(fh)
+
+    else:  # Whatever ...
+        sys.stderr.write(__doc__)
+        sys.exit(1)
+
+    return fl
+
+
+def concatenate_files(flist):
+    """Iterates over a list of files and yields each line sequentially.
+    """
+
+    for fhandle in flist:
+        for line in fhandle:
+            yield line
+
+
+if __name__ == '__main__':
+    # Check Input
+    pdbfh = check_input(sys.argv[1:])
+
+    # Do the job
+    new_pdb = concatenate_files(pdbfh)
+
+    try:
+        sys.stdout.write(''.join(new_pdb))
+        sys.stdout.flush()
+    except IOError:
+        # This is here to catch Broken Pipes
+        # for example to use 'head' or 'tail' without
+        # the error message showing up
+        pass
+
+    # last line of the script
+    # We can close it even if it is sys.stdin
+    pdbfh.close()
+    sys.exit(0)
