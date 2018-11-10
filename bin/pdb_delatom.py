88fa54242726a170df3d71443ae7ca60ca867f2f
João Rodrigues <j.p.g.l.m.rodrigues@gmail.com>
2018-11-09 23:10:21 -0600

    Added encoding line to support Py 2.x and non-ASCII characters in the header.

--- bin/pdb_delatom.py
+++ bin/pdb_delatom.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 #
 # Copyright 2018 João Pedro Rodrigues
 #
--- bin/pdb_sort.py
+++ bin/pdb_sort.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 #
 # Copyright 2018 João Pedro Rodrigues & João M. C. Teixeira
 #
