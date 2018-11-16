8565d0f8e321646d9d82fd4f0335d5d9abead356
joaomcteixeira <joaomcteixeira@gmail.com>
2018-11-16 16:25:20 +0100

    test added test validade

---
+++ tests/test_pdb_validate.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2018 João Pedro Rodrigues
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+Unit Tests for `pdb_validate`.
+"""
+
+import os
+import sys
+import unittest
+
+from config import data_dir, output_dir
+from utils import OutputCapture
+
+
+class TestTool(unittest.TestCase):
+    """Generic class for testing tools.
+    """
+
+    def setUp(self):
+        # Dynamically import the module
+        name = 'bin.pdb_validate'
+        self.module = __import__(name, fromlist=[''])
+    
+    def read_prepare(self, input_file):
+        """
+        Prepares input and output common to the different tests.
+        """
+        
+        with OutputCapture() as output:
+            try:
+                self.module.main()
+            except SystemExit as e:
+                self.retcode = e.code
+
+        self.stdout = output.stdout
+        self.stderr = output.stderr
+        
+        return
+    
+    def test_valid_1(self):
+        """pdb_validate - pdb okay"""
+        
+        input_file = os.path.join(data_dir, 'nano.pdb')
+        
+        sys.argv = ['', input_file]  # simulate
+        # Execute the script
+        
+        self.read_prepare(input_file)
+        
+        self.assertEqual(self.retcode, 0)  # ensure the program exited gracefully.
+        self.assertEqual(len(self.stderr), 0)  # no errors
+        self.assertEqual(self.stdout[0], "It *seems* everything is OK.")
+    
+    def test_valid_2(self):
+        """pdb_validate - pdb not okay"""
+        
+        input_file = os.path.join(data_dir, 'full_example.pdb')
+        
+        sys.argv = ['', input_file]  # simulate
+        # Execute the script
+        
+        self.read_prepare(input_file)
+        
+        self.assertEqual(self.retcode, 0)  # ensure the program exited gracefully.
+        self.assertEqual(len(self.stderr), 6)  # with errors
+        self.assertEqual(self.stderr[:3],
+                         ["[!] Line 10 is short: 67 < 80",
+                         "[!] Line 12 is short: 24 < 80",
+                         "[!] Line 13 is long: 81 > 80"])
+    
+    def test_FileNotFound(self):
+        """pdb_validate - file not found"""
+
+        # Error (file not found)
+        sys.argv = ['', os.path.join(data_dir, 'not_there.pdb')]
+        # Execute the script
+        with OutputCapture() as output:
+            try:
+                self.module.main()
+            except SystemExit as e:
+                retcode = e.code
+
+        stdout = output.stdout
+        stderr = output.stderr
+
+        self.assertEqual(retcode, 1)  # ensure the program exited gracefully.
+        self.assertEqual(len(stdout), 0)  # no output
+        self.assertEqual(stderr[0][:39], "ERROR!! File not found or not readable:")
+    
