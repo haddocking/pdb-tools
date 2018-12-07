4aad2199973e6663b86ddeb749fe26d5c91e054c
João Rodrigues <j.p.g.l.m.rodrigues@gmail.com>
2018-12-07 01:03:24 -0500

    Added new tools to better select/delete residues and chain sequentially large structures

---
+++ pdbtools/pdb_chainbows.py
@@ -0,0 +1,150 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2018 João Pedro Rodrigues
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+Sets chain identifiers automatically starting from A, and sequentially from then
+on when the residue count exceeds 9999. Useful to use with pdb_splitchain to
+quickly make very large PDBs amenable to the format (i.e. solvated trajectories).
+
+Usage:
+    python pdb_chainbows.py <pdb file>
+
+Example:
+    python pdb_chainbows.py 1CTF.pdb
+
+This program is part of the `pdb-tools` suite of utilities and should not be
+distributed isolatedly. The `pdb-tools` were created to quickly manipulate PDB
+files using the terminal, and can be used sequentially, with one tool streaming
+data to another. They are based on old FORTRAN77 code that was taking too much
+effort to maintain and compile. RIP.
+"""
+
+import os
+import sys
+
+__author__ = "Joao Rodrigues"
+__email__ = "j.p.g.l.m.rodrigues@gmail.com"
+
+
+def check_input(args):
+    """Checks whether to read from stdin/file and validates user input/options.
+    """
+
+    # Defaults
+    fh = sys.stdin  # file handle
+
+    if not len(args):
+        # Reading from pipe with default option
+        if sys.stdin.isatty():
+            sys.stderr.write(__doc__)
+            sys.exit(1)
+
+    elif len(args) == 1:
+        if not os.path.isfile(args[0]):
+            emsg = 'ERROR!! File not found or not readable: \'{}\'\n'
+            sys.stderr.write(emsg.format(args[0]))
+            sys.stderr.write(__doc__)
+            sys.exit(1)
+
+        fh = open(args[0], 'r')
+
+    else:  # Whatever ...
+        emsg = 'ERROR!! Script takes 1 argument, not \'{}\'\n'
+        sys.stderr.write(emsg.format(len(args)))
+        sys.stderr.write(__doc__)
+        sys.exit(1)
+
+    return fh
+
+
+def pad_line(line):
+    """Helper function to pad line to 80 characters in case it is shorter"""
+    size_of_line = len(line)
+    if size_of_line < 80:
+        padding = 80 - size_of_line + 1
+        line = line.strip('\n') + ' ' * padding + '\n'
+    return line[:81]  # 80 + newline character
+
+
+def set_chains(fhandle):
+    """Sets the chain identifier column sequentially.
+    """
+
+    res_uid = None
+    res_counter = 0
+
+    chain_id_list = list('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+    chain_id_list.reverse()
+    max_chains = len(chain_id_list)
+    _pad_line = pad_line
+    records = ('ATOM', 'HETATM', 'TER', 'ANISOU')
+    for line in fhandle:
+
+        if line.startswith(records):
+            line = _pad_line(line)
+
+            yield line[:21] + chain_id_list[-1] + line[22:]
+
+            cur_res = line[17:26]
+            if cur_res != res_uid:
+                res_uid = cur_res
+                res_counter += 1
+
+            if res_counter == 9999:
+                res_counter = 0
+                chain_id_list.pop()
+                if not chain_id_list:
+                    emsg = 'ERROR!! Ran out of chains ({}) to place...too big!\n'
+                    sys.stderr.write(emsg.format(max_chains))
+                    sys.exit(1)
+
+        else:
+            yield line
+
+
+def main():
+    # Check Input
+    pdbfh = check_input(sys.argv[1:])
+
+    # Do the job
+    new_pdb = set_chains(pdbfh)
+
+    try:
+        _buffer = []
+        _buffer_size = 5000  # write N lines at a time
+        for lineno, line in enumerate(new_pdb):
+            if not (lineno % _buffer_size):
+                sys.stdout.write(''.join(_buffer))
+                _buffer = []
+            _buffer.append(line)
+
+        sys.stdout.write(''.join(_buffer))
+        sys.stdout.flush()
+    except IOError:
+        # This is here to catch Broken Pipes
+        # for example to use 'head' or 'tail' without
+        # the error message showing up
+        pass
+
+    # last line of the script
+    # We can close it even if it is sys.stdin
+    pdbfh.close()
+    sys.exit(0)
+
+
+if __name__ == '__main__':
+    main()
