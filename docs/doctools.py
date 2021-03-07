c92298ffbc0152488f9649fce41613d72d444d50
João Rodrigues <j.p.g.l.m.rodrigues@gmail.com>
2021-03-07 15:26:57 -0800

    Auto-generate documentation page.

--- docs/doctools.py
+++ docs/doctools.py
@@ -7,47 +7,51 @@ Scrapes tool headers for documentation.
 import importlib
 from pathlib import Path
 
-with open('descriptions.md', 'w') as handle:
+with open('index.md', 'w') as handle:
 
-	import pdbtools
-	modfile = Path(pdbtools.__file__)
-	for f in modfile.parent.iterdir():
-		# ignore __init__.py and others.
-		if f.stem.startswith('_') or f.suffix != '.py':
-			continue
+    with open('index.md.template') as template:
+        print(template.read(), file=handle)
 
-		# Dynamically import tool to get __doc__
-		name = f.stem
-		try:
-			tool = importlib.import_module(f'pdbtools.{name}')
-		except ModuleNotFoundError:
-			print(f'Could not import module: {name}')
-			continue
+    import pdbtools
+    modfile = Path(pdbtools.__file__)
+    for f in modfile.parent.iterdir():
+        # ignore __init__.py and others.
+        if f.stem.startswith('_') or f.suffix != '.py':
+            continue
 
-		# Parse documentation from docstrings
-		# Preserve white-space as best as possible.
-		# First non-empty line is always short description.
-		# Last lines are always licensing disclaimer
-		summary = None
-		long_description = []
+        # Dynamically import tool to get __doc__
+        name = f.stem
+        try:
+            tool = importlib.import_module(f'pdbtools.{name}')
+        except ModuleNotFoundError:
+            print(f'Could not import module: {name}')
+            continue
 
-		doctext = tool.__doc__.replace('<', '&lt;').replace('>', '&gt;')
-		for line in doctext.split('\n'):
-			if summary is None and not line.strip():
-				continue
-			if line.startswith('This program is part of the'):
-				break
-			elif summary is None:
-				summary = line
-			else:
-				long_description.append(line)
-	
-		long_description = '\n'.join(long_description)
-		print('<div style="margin-bottom: 1em;">')
-		print('<details>')
-		print(f"<summary><b>{name}</b><p>{summary}</p></summary>")
-		print(f'<span style="font-family: monospace; white-space: pre;">{long_description}</span>')
-		print('</details>')
-		print('</div>')
+        # Parse documentation from docstrings
+        # Preserve white-space as best as possible.
+        # First non-empty line is always short description.
+        # Last lines are always licensing disclaimer
+        summary = None
+        long_description = []
 
+        doctext = tool.__doc__.replace('<', '&lt;').replace('>', '&gt;')
+        for line in doctext.split('\n'):
+            if summary is None and not line.strip():
+                continue
+            if line.startswith('This program is part of the'):
+                break
+            elif summary is None:
+                summary = line
+            else:
+                long_description.append(line)
 
+        long_description = '\n'.join(long_description)
+        print('<div style="margin-bottom: 1em;">', file=handle)
+        print('<details>', file=handle)
+        print(f"<summary><b>{name}</b><p>{summary}</p></summary>", file=handle)
+        print(
+            f'<span style="font-family: monospace; white-space: pre;">{long_description}</span>',
+            file=handle
+        )
+        print('</details>', file=handle)
+        print('</div>', file=handle)
