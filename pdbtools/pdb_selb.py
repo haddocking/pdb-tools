cc39ec33ab265020a70b7305aedf6c969b20bbaf
ahmedselim2017 <ahmedselimuzum@gmail.com>
2024-09-20 09:23:53 +0300

    Changed copyright of pdb_selb.py

--- pdbtools/pdb_selb.py
+++ pdbtools/pdb_selb.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2024 Ahmed Selim Üzüm
+# Copyright 2024 João Pedro Rodrigues
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,7 +14,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
 """
 Selects atoms according to their B-factor value.
 
