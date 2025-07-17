29a0fd2f41e04514278d5bf2c7aa9fe96374dec7
Rodrigo V. Honorato <r.vargashonorato@uu.nl>
2025-07-17 15:44:07 +0200

    update classifiers

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ https://github.com/pypa/sampleproject
 # Always prefer setuptools over distutils
 from setuptools import setup, find_packages
 from os import listdir, path
+
 # io.open is needed for projects that support Python 2.7
 # It ensures open() defaults to text mode with universal newlines,
 # and accepts an argument to specify the text encoding
@@ -17,54 +18,39 @@ from io import open
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+with open(path.join(here, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
 # Collect names of bin/*py scripts
 # e.g. 'pdb_intersect=bin.pdb_intersect:main',
-binfiles = listdir(path.join(here, 'pdbtools'))
-bin_py = [f[:-3] + '=pdbtools.' + f[:-3] + ':main' for f in binfiles
-          if f.endswith('.py')]
+binfiles = listdir(path.join(here, "pdbtools"))
+bin_py = [
+    f[:-3] + "=pdbtools." + f[:-3] + ":main" for f in binfiles if f.endswith(".py")
+]
 
 setup(
-    name='pdb-tools',  # Required
-    version='2.5.1',  # Required
-    description='A swiss army knife for PDB files.',  # Optional
+    name="pdb-tools",  # Required
+    version="2.5.1",  # Required
+    description="A swiss army knife for PDB files.",  # Optional
     long_description=long_description,  # Optional
-    long_description_content_type='text/markdown',  # Optional (see note above)
-    url='http://bonvinlab.org/pdb-tools',  # Optional
-    author='Joao Rodrigues',  # Optional
-    author_email='j.p.g.l.m.rodrigues@gmail.com',  # Optional
-    license='Apache Software License, version 2',
-    classifiers=[  # Optional
-        # How mature is this project? Common values are
-        #   3 - Alpha
-        #   4 - Beta
-        #   5 - Production/Stable
-        'Development Status :: 5 - Production/Stable',
-        #'Development Status :: 4 - Beta',
-
-        # Indicate who your project is intended for
-        'Intended Audience :: Science/Research',
-        'Topic :: Scientific/Engineering',
-        'Topic :: Scientific/Engineering :: Chemistry',
-        'Topic :: Scientific/Engineering :: Bio-Informatics',
-
-
-        # Pick your license as you wish
-        'License :: OSI Approved :: Apache Software License',
-
-        # Specify the Python versions you support here. In particular, ensure
-        # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
+    long_description_content_type="text/markdown",  # Optional (see note above)
+    url="http://bonvinlab.org/pdb-tools",  # Optional
+    author="Joao Rodrigues",  # Optional
+    author_email="j.p.g.l.m.rodrigues@gmail.com",  # Optional
+    license="Apache Software License, version 2",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Chemistry",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
     ],
-
-    keywords='bioinformatics protein structural-biology pdb',  # Optional
-
+    keywords="bioinformatics protein structural-biology pdb",  # Optional
     # You can just specify package directories manually here if your project is
     # simple. Or you can use find_packages().
     #
@@ -75,7 +61,6 @@ setup(
     #   py_modules=["my_module"],
     #
     packages=find_packages(),  # Required
-
     # To provide executable scripts, use entry points in preference to the
     # "scripts" keyword. Entry points provide cross-platform support and allow
     # `pip` to create the appropriate form of executable for the target
@@ -84,11 +69,9 @@ setup(
     # For example, the following would provide a command called `sample` which
     # executes the function `main` from this package when invoked:
     entry_points={  # Optional
-        'console_scripts': bin_py,
+        "console_scripts": bin_py,
     },
-
     # scripts=[path.join('bin', f) for f in listdir(path.join(here, 'bin'))],
-
     # List additional URLs that are relevant to your project as a dict.
     #
     # This field corresponds to the "Project-URL" metadata fields:
@@ -99,10 +82,9 @@ setup(
     # maintainers, and where to support the project financially. The key is
     # what's used to render the link text on PyPI.
     project_urls={  # Optional
-        'Bug Reports': 'https://github.com/haddocking/pdb-tools/issues',
-        'Source': 'https://github.com/haddocking/pdb-tools',
+        "Bug Reports": "https://github.com/haddocking/pdb-tools/issues",
+        "Source": "https://github.com/haddocking/pdb-tools",
     },
-
     # Test Suite
-    test_suite='tests.test_all',
+    test_suite="tests.test_all",
 )
